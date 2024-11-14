f968ef6756e71dd1eb8108469443ff378cc73794
envy1201 <106316962+envy1201@users.noreply.github.com>
2024-11-14 17:54:25 +0300

    Delete js directory

--- js/plugins/remove_current_action_of_null_fix.js
+++
@@ -1,9 +0,0 @@
-BattleManager.processForcedAction = function() {
-    if (this._actionForcedBattler) {
-        this._turnForced = true;
-        this._subject = this._actionForcedBattler;
-        this._actionForcedBattler = null;
-        this.startAction();
-        if (!!this._subject) this._subject.removeCurrentAction();
-    }
-};
